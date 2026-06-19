56d4ffea6a34d882e5d47124af27cbc9b1c72ab7
agalal17 <ayman.galal@nourconsulting.co.uk>
2026-06-20 00:07:24 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AdamsLab from './App'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><AdamsLab /></React.StrictMode>
+)
